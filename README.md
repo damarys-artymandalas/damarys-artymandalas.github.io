c33645a8662d4f567a62528a941032f59ab35677
andre arima <andre.hiromi.arima@gmail.com>
2021-01-21 12:40:28 -0300

    no message

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Artymandalas
+# DamarysArtymandalas
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.6.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('artymandalas app is running!');
+    expect(page.getTitleText()).toEqual('damarys-artymandalas app is running!');
   });
 
   afterEach(async () => {
